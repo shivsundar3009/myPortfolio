e01eabd9b5b79adfe67d7812d03453181cc488ca
shivsundar3009 <shivsundar3009@gmail.com>
2024-01-21 22:07:34 +0530

    basic components structure creation

---
+++ myPortfolio/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+    <div> left</div>
+    <div>
+      <a href="">Home</a>
+      <a href="">About</a>
+      <a href="">Projects</a>
+      <a href="">Contact</a>
+
+    </div>
+    </>
+  )
+}
+
+export default Header
